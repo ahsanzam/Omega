0a347cd75deb342990271194a258266f8f5ca83c
Phillip Nazarian <pmnazari@usc.edu>
2018-07-02 12:42:18 -0700

    that's depressing

--- js/InterviewApplication.js
+++ js/InterviewApplication.js
@@ -51,7 +51,7 @@ InterviewApplication.prototype.triggerInterviewEvent = function(eventName)
 	// iterate through all interview questions
 	for (let i in this.interviewees)
 	{
-		let intervieweeQuestions = interviewees[i];
+		let intervieweeQuestions = this.interviewees[i];
 		for (let j in intervieweeQuestions)
 		{
 			question = intervieweeQuestions[j];
